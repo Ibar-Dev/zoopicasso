e68393fb0c575e304b71e57dfbe61ec143f819a2
Ibar-Dev <ibarvivas@gmail.com>
2026-07-14 14:48:52 +0200

    Refactor code structure for improved readability and maintainability

--- Facturas.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0generar_para_email"
-uv run main.py
-if errorlevel 1 pause
--- generar_para_email/tests/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Helpers para tests E2E
-"""
-from .db_helper import DBHelper
-from .file_helper import FileHelper
-
-__all__ = ['DBHelper', 'FileHelper']
